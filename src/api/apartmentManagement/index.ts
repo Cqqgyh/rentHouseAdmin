0853492baaad9f061ebb3b5cd21d7e092dc2a535
cqq <945327638@qq.com>
2023-07-08 18:14:21 +0800

    feat: 增加api接口机、type定义

--- src/api/apartmentManagement/index.ts
+++ src/api/apartmentManagement/index.ts
@@ -2,6 +2,9 @@ import http from '@/utils/http'
 import {
   ApartmentInterface,
   ApartmentListQueryInterface,
+  FacilityInfoInterface,
+  FeeInfoInfoInterface,
+  LabelInfoInterface,
   RegionInterface,
 } from '@/api/apartmentManagement/types'
 import { PageRes } from '@/api/types'
@@ -27,6 +30,7 @@ export function getProvinceList() {
 }
 /**
  * @description 查询城市列表
+ * @param provinceId
  */
 export function getCityList(provinceId: number | string) {
   return http.get<RegionInterface[]>(
@@ -35,6 +39,7 @@ export function getCityList(provinceId: number | string) {
 }
 /**
  * @description 查询区域列表
+ * @param cityId
  */
 export function getDistrictList(cityId: number | string) {
   return http.get<RegionInterface[]>(
@@ -43,6 +48,8 @@ export function getDistrictList(cityId: number | string) {
 }
 /**
  * @description 根据id修改公寓的发布状态
+ * @param id
+ * @param status
  */
 export function updateApartmentReleaseStatus(
   id: number | string,
@@ -51,4 +58,36 @@ export function updateApartmentReleaseStatus(
   return http.post(
     `/admin/apartment/updateReleaseStatusById?id=${id}&status=${status}`,
   )
+}
+/**
+ * @description 保存或更新公寓信息
+ * @param params
+ */
+export function saveOrUpdateApartment(params: Partial<ApartmentInterface>) {
+  return http.post(`/admin/apartment/saveOrUpdate`, params)
+}
+/**
+ * @description 根据id删除公寓
+ * @param id
+ */
+export function deleteApartmentById(id: number | string) {
+  return http.delete(`/admin/apartment/removeById?id=${id}`)
+}
+/**
+ * @description 查询配套信息列表
+ */
+export function getFacilityInfoList() {
+  return http.get<FacilityInfoInterface[]>(`/admin/facility/list`)
+}
+/**
+ * @description 查询标签信息列表
+ */
+export function getLabelInfoList() {
+  return http.get<LabelInfoInterface[]>(`/admin/label/list`)
+}
+/**
+ * @description 查询杂费信息列表
+ */
+export function getFeeInfoList() {
+  return http.get<FeeInfoInfoInterface[]>(`/admin/fee/list`)
 }
--- src/api/apartmentManagement/types.ts
+++ src/api/apartmentManagement/types.ts
@@ -13,22 +13,64 @@ export interface ApartmentInterface {
   id: number
   name: string
   introduction: string
-  districtId: number
+  districtId: number | string
   districtName: string
-  cityId: number
+  cityId: number | string
   cityName: string
-  provinceId: number
+  provinceId: number | string
   provinceName: string
   addressDetail: string
-  latitude: string
-  longitude: string
+  latitude: number | string
+  longitude: number | string
   phone: string
   isRelease: string
   totalRoomCount: number
   freeRoomCount: number
+  // 	公寓配套id
+  facilityInfoIds?: []
+  // 	公寓标签id
+  labelIds?: []
+  // 	公寓杂费值id
+  feeValueIds?: []
+  // 图片信息
+  graphVoList?: [
+    {
+      name: ''
+      url: ''
+    },
+  ]
 }
 // 省市区
 export interface RegionInterface {
   id: number
   name: string
+}
+export interface AddressOptionsInterface {
+  label: string
+  value: string
+  location?: { lng: number; lat: number }
+}
+// 配套信息
+export interface FacilityInfoInterface {
+  id: number
+  type: string
+  name: string
+  icon: string | null
+}
+// 标签信息
+export interface LabelInfoInterface {
+  id: number
+  type: string
+  name: string
+}
+// 杂费值信息
+export interface FeeInfoInfoInterface {
+  id: number
+  name: string
+  feeValueList: {
+    id: number
+    name: string
+    unit: string
+    feeKeyId: number
+  }[]
 }
