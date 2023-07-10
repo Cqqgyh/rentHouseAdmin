f0075284fe62378db02f3849ae74f813ec1e772e
cqq <945327638@qq.com>
2023-07-10 14:44:21 +0800

    perf: 取消无用默认参数

--- src/views/apartmentManagement/apartmentManagement/components/addOrEditApartment.vue
+++ src/views/apartmentManagement/apartmentManagement/components/addOrEditApartment.vue
@@ -223,10 +223,10 @@ const formData = ref<FormDataInstance>({
   labelIds: [],
   feeValueIds: [],
   graphVoList: [
-    {
-      name: 'food.jpeg',
-      url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
-    },
+    // {
+    //   name: 'food.jpeg',
+    //   url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+    // },
   ] as UploadFile[],
 })
 // 表单验证规则
