aa25e0320598246be838f5c24b750267b2adf82c
cqq <945327638@qq.com>
2023-07-11 18:15:52 +0800

    perf: 增加ts-declare

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,8 @@ declare module '*.vue' {
   export default component
 }
 declare module 'mockjs'
+declare module 'virtual:svg-icons-names' {
+  // eslint-disable-next-line
+  const iconsNames: string[]
+  export default iconsNames
+}
