07d0c9e69bc6261285c7cbaefae8196326f7394c
cqq <945327638@qq.com>
2023-10-08 16:56:38 +0800

    feat: 优化注释

--- src/store/modules/tabsBar.ts
+++ src/store/modules/tabsBar.ts
@@ -1,8 +1,8 @@
 /*
  * @Description: tabsBar 仓库
- * @Autor: 李海波
+ * @Autor: 浅笑
  * @Date: 2023-03-03 15:37:08
- * @LastEditors: 1547702880@@qq.com
+ * @LastEditors: 945327638@qq.com
  * @LastEditTime: 2023-03-27 15:58:56
  */
 import { defineStore } from 'pinia'
